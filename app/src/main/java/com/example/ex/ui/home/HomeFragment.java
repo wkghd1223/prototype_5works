53fe9fbb023445e41c3e9f445758f40924962be5
wkghd1223 <wkghd951223@gmail.com>
2019-11-10 17:19:28 +0900

    20191110업데이트 | HomeFragmet 수정 HomeFragment와 webView를 연결

--- app/src/main/java/com/example/ex/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ex/ui/home/HomeFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +17,26 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ex.R;
+import com.example.ex.ui.serach.SerachViewModel;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private SerachViewModel mViewModel;
+    private WebView mWebView;
+    private WebSettings mWebSettings;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        mWebView = root.findViewById(R.id.webViewHome);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://www.google.com/");
         return root;
     }
 }
